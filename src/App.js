f34fb9ffc90c05d9d0d96039defc07658fd0f4fc
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-05-23 12:20:31 -0300

    initialState

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Jesus loves you!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
